817bf87799c441f2f6893a52c23867e3e290c21d
Sean Clifford <sean.clifford@myob.com>
2021-01-29 11:38:34 +1300

    Try removing upgrade aws-sam-cli
    I think the new codebuild images have a version we can use.

--- bin/Prepare_Lambda_Build.ps1
+++ bin/Prepare_Lambda_Build.ps1
@@ -1,4 +1,1 @@
-pip install --upgrade aws-sam-cli
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-
 sam --version    
--- bin/Prepare_Lambda_Deploy.ps1
+++ bin/Prepare_Lambda_Deploy.ps1
@@ -1,9 +1,4 @@
 install-package AWS.Tools.SecurityToken -Force
 install-package AWS.Tools.EC2 -Force
 
-pip install --upgrade aws-sam-cli
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-
 sam --version
-
-
